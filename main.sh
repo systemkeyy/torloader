79f1a9ec184ddc7c0e31bb2340ccdd94b45ebda3
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-03-12 23:34:11 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -28,7 +28,7 @@ sleep 1
 clear &&
 echo -ne $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'TOR SOURCE CLONED - GIVE PERMISSION TO INSTALL DEVTOOLS AND WE WILL CONTINUE'\033[0m' 
 sleep 5
-chmod +x dep.sh && chmod +x setup.sh &&
+chmod +x dep.sh && chmod +x setup.sh && chmod +x backend.sh &&
 sleep 1 &&
 echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'JUST CHECKING WHATS MISSING FOR FRONTEND - STARTING INSTALL VERY SOON'\033[0m' 
 echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'CHECKING....'\033[0m' 
@@ -37,7 +37,7 @@ sleep 3
 clear 
 read -p "INSTALL MISSING PARTS AND INSTALL LOADBALANCER? Yes/No " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
-then bash frontload.sh;
+then cd && cd torloader && bash dep.sh;
 ;
  else
     echo "Fuck off then"
