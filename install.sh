103a5e4c1a145737b4ee9235eb4b6e6c9ba6b31a
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-03-12 23:49:56 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,61 @@
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'AUTOMATIC ONION LOADBALACNER - FRONT END SERVICE VERSION 1.0 - PRESS ENTER IF SCRIPT SLEEPS!'\033[0m' 
+echo "S"
+sleep 0.1
+echo "Y"
+sleep 0.1
+echo "S"
+sleep 0.1
+echo "T"
+sleep 0.1
+echo "E"
+sleep 0.1
+echo "M"
+sleep 0.1
+echo "K"
+sleep 0.1
+echo "e"
+sleep 0.1
+echo "y"
+sleep 0.1
+echo "y"
+sleep 5 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'BOOTING MAIN.....'.';
+echo -ne '>                       [5%]\r'
+sleep 0.1
+echo -ne '>>                      [10%]\r'
+sleep 0.2
+echo -ne '>>>>                    [20%]\r'
+sleep 0.1
+echo -ne '>>>>>                   [25%]\r'
+sleep 0.1
+echo -ne '>>>>>>                  [30%]\r'
+sleep 0.1
+echo -ne '>>>>>>>                 [35%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>                [40%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>               [45%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>              [50%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>             [55%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>            [60%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>           [65%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>          [70%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>         [75%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>        [80%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>       [85%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>      [90%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>     [95%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>>>   [100%]\r.'
+sleep 2
+chmod +x main.sh && bash main.sh -y
