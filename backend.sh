d2c179993442fe07ca5ebbdd7802012053b9dc7f
Systemkeyy <74800251+systemkeyy@users.noreply.github.com>
2022-03-12 23:32:50 +0100

    Create backend.sh

---
+++ backend.sh
@@ -0,0 +1,71 @@
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'AUTOMATIC ONION LOADBALACNER - FRONT END SERVICE VERSION 1.0 - PRESS ENTER IF SCRIPT SLEEPS!'\033[0m' 
+echo "S"
+sleep 0.1
+echo "Y"
+sleep 0.1
+echo "S"
+sleep 0.1
+echo "T"
+sleep 0.1
+echo "E"
+sleep 0.1
+echo "M"
+sleep 0.1
+echo "K"
+sleep 0.1
+echo "e"
+sleep 0.1
+echo "y"
+sleep 0.1
+echo "y"
+sleep 5 &&
+echo -e $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'EVERYTHING IS WORKING - PLEASE GO TO YOUR BACKEND SERVICE AND PROMPT git clone https://github.com/systemkeyy/torloaderv2!'.';
+echo -ne '>                       [5%]\r'
+sleep 0.1
+echo -ne '>>                      [10%]\r'
+sleep 0.2
+echo -ne '>>>>                    [20%]\r'
+sleep 0.1
+echo -ne '>>>>>                   [25%]\r'
+sleep 0.1
+echo -ne '>>>>>>                  [30%]\r'
+sleep 0.1
+echo -ne '>>>>>>>                 [35%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>                [40%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>               [45%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>              [50%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>             [55%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>            [60%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>           [65%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>          [70%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>         [75%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>        [80%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>       [85%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>      [90%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>     [95%]\r'
+sleep 0.1
+echo -ne '>>>>>>>>>>>>>>>>>>>>>   [100%]\r.'
+sleep 2
+clear &&
+echo -ne $'\e[1;91m[\e[0m\e[1;77m+\e[0m\e[1;91m]\e[1;32m'FRONTEND IS DONE!'\033[0m' 
+sleep 5 &&
+clear 
+read -p "CONFIG FOR FRONTEND IS DONE - CONTINUE TO BACKEND? Yes/No " prompt
+if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "Yes" ]]
+then echo "See you on backend" && sleep 3 && exit;
+;
+ else
+    echo "Fuck off then"
+ fi
